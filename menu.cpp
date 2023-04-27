2121c35c9d706356941aa22553d85c8fabc00d3a
SM1SHi <sergejpro126@gmail.com>
2023-04-27 14:27:29 +0200

    added menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include "utils.h"
 
-std::string menu() {
-	uint8_t op;
+void menu() {
+	uint32_t op;
 	std::string loginID;
 	do {
-		std::cout << "Welcome, please select an option:" << std::endl;
+		std::cout << "Please select an option:" << std::endl;
 		std::cout << "1. Register " << std::endl;
 		std::cout << "2. Login" << std::endl;
 		std::cout << "3. Exit" << std::endl;
@@ -17,14 +17,40 @@ std::string menu() {
 		case 1:
 			std::cout << "Register selected." << std::endl;
 			registerUser();
-			break;
+			continue;
 		case 2:
 			std::cout << "Login selected." << std::endl;
 			loginID = login();
+			do {
+				std::cout << std::endl
+					<< "Select option: " << std::endl
+					<< "1. Check account balance" << std::endl
+					<< "2. Transfer assets" << std::endl
+					<< "3. Check account history" << std::endl
+					<< "4. Return to previous menu" << std::endl;
+					
+				std::cout << "> ";
+				unsigned int choice;
+				std::cin >> choice;
+
+				switch (choice) {
+				case 1:
+					checkAccountBalance(loginID, 1);
+					continue;
+				case 2:
+					transferAssets(loginID);
+					continue;
+				case 3:
+					std::cout << "not implemented yet" << std::endl;
+					continue;
+				case 4:
+					break;
+				}
+			} while (true);
 			break;
 		case 3:
 			std::cout << "exiting..." << std::endl;
-			exit(0);
+			break;
 		default:
 			if (std::cin.fail()) {
 				std::cin.clear();
